f21465efac9c87d82e121cd2853b34c2a7b0f8e3
Dharshan senthil <131781900+Dharshan209@users.noreply.github.com>
2025-05-15 21:15:48 +0530

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io("http://game-client.railway.internal"); // or your deployed URL
+const socket = io("http://game-client-production-c665.up.railway.app"); // or your deployed URL
 
 export default socket;
