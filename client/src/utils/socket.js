6a23882a1e2e356a2f176e5d381f52f99ee3541f
Dharshan senthil <131781900+Dharshan209@users.noreply.github.com>
2025-05-15 21:10:18 +0530

    ..

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:3000"); // or your deployed URL
+const socket = io("http://game-client.railway.internal"); // or your deployed URL
 
 export default socket;
