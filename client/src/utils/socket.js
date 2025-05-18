c3ea98695a3a67a7463f0146afe839a61bbe56b5
Dharshan senthil <131781900+Dharshan209@users.noreply.github.com>
2025-05-18 23:53:37 +0530

    ..

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_BACKEND_URL, {
-  transports: ['websocket'],
-});
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 export default socket;
